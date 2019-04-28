203cbfd623a021d4e40189c780da3f86a2ccdd56
palicao <palicao@gmail.com>
2019-04-28 16:49:12 +0200

    add a setup.py, fixing (partially) #7

---
+++ setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='LogClouseau',
+    packages=find_packages(exclude=['tests']),
+    version='0.1.0',
+    description='LogClouseau inspects your logs, extracts information and '
+                'sends alerts according to your conditions',
+    long_description=readme,
+    author='Alessandro Balasco',
+    author_email='alessandro.balasco@gmail.com',
+    url='https://github.com/palicao/logclouseau',
+    keywords=[
+        'log',
+        'inspect',
+        'alert',
+    ],
+    install_requires=[
+        'toml',
+        'slackclient',
+        'tailer',
+        'click',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
+    ],
+    entry_points={
+        'console_scripts': ['logclouseau=src.command:main'],
+    }
+)
